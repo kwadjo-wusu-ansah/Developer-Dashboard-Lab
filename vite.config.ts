116bc6c1a911522014da062d7f59d36b699c3e56
KwadjoW-A <wkjwusu@gmail.com>
2026-02-11 09:38:21 +0000

    add(vite.config)

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Replace 'your-repo-name' with the name of your GitHub repository
+  base: '/Developer-Dashboard-Lab/', 
+})
